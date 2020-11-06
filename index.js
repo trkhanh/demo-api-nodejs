9395ba43a21a014b4b8c5899dfff5615d4c381ff
Khanh <tran_khanh@outlook.com>
2020-11-06 17:28:56 +0700

    update

--- index.js
+++ index.js
@@ -1,39 +1,15 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const router = express.Router();
-const app = express();
-const port = 3000;
-const cors = require('cors');
+const cors = require("cors");
 
 app.use(cors());
-var fs = require('fs');
-var http = require('http');
-var https = require('https');
-// var privateKey  = fs.readFileSync('./selfsigned.key', 'utf8');
-// var certificate = fs.readFileSync('./selfsigned.crt', 'utf8');
 
-// var credentials = {key: privateKey, cert: certificate};
+const express = require("express");
+const path = require("path");
+const PORT = process.env.PORT || 5000;
 
-var httpServer = http.createServer(app);
-var httpsServer = https.createServer(app);
-
-httpServer.listen(3000);
-httpsServer.listen(3001);
-
-//Here we are configuring express to use body-parser as middle-ware.
-
-app.post('/test', function(req, res){
-
-	console.log('RUN');
-	res.end('1');
-})	        
-
-
-
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-
-
+express()
+  .use(cors())
+  .use(express.static(path.join(__dirname, "public")))
+  .set("views", path.join(__dirname, "views"))
+  .set("view engine", "ejs")
+  .get("/", (req, res) => res.render("pages/index"))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
