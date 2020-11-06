3d125bc77925fdc2871d933c6e97ab39e9be3911
Khanh <tran_khanh@outlook.com>
2020-11-06 17:21:24 +0700

    update

--- server.js
+++ server.js
@@ -9,13 +9,13 @@ app.use(cors());
 var fs = require('fs');
 var http = require('http');
 var https = require('https');
-var privateKey  = fs.readFileSync('./selfsigned.key', 'utf8');
-var certificate = fs.readFileSync('./selfsigned.crt', 'utf8');
+// var privateKey  = fs.readFileSync('./selfsigned.key', 'utf8');
+// var certificate = fs.readFileSync('./selfsigned.crt', 'utf8');
 
-var credentials = {key: privateKey, cert: certificate};
+// var credentials = {key: privateKey, cert: certificate};
 
 var httpServer = http.createServer(app);
-var httpsServer = https.createServer(credentials, app);
+var httpsServer = https.createServer(app);
 
 httpServer.listen(3000);
 httpsServer.listen(3001);
